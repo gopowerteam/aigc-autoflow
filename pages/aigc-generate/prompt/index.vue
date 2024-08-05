f9f73ff1935143bb59d3bcbe1f0775c42313ca24
HuangRui <hr837@163.com>
2024-08-05 23:58:18 +0800

    fix: 试用renderTable,renderForm

--- pages/aigc-generate/prompt/index.vue
+++ pages/aigc-generate/prompt/index.vue
@@ -1,6 +1,24 @@
 <script lang="tsx" setup>
 import { defineColumns, defineTableLoad } from '@gopowerteam/table-render'
+import { FormRender } from '@gopowerteam/form-render'
 import type { Prompt } from '@/drizzle/schemas'
+
+const promptTable = ref(null)
+
+const form2 = defineForm<Prompt>([
+  {
+    key: 'tag',
+    title: '标签',
+    rule: [{ required: true, message: '请输入标签名称' }],
+    render: r => r.input(),
+  },
+  {
+    key: 'template',
+    title: '模板内容',
+    rule: [{ required: true, message: '请输入模板内容' }],
+    render: r => r.textarea(),
+  },
+])
 
 definePageMeta({
   layout: 'workspace',
@@ -36,9 +54,19 @@ const columns = defineColumns<Prompt>([{
   title: '操作',
   render: r => r.button([{
     content: '更新',
-    // onClick(record) {
-    //   // return requestUpdateTask(record)
-    // },
+    onClick(record) {
+      // 错误的useTable，需要使用ref
+      promptTable.value.edit({
+        record,
+        form: form2,
+      }).then(async (data) => {
+        await $request(`/api/prompt/${record.id}`, {
+          method: 'PUT',
+          body: data,
+        })
+        Message.success('更新成功')
+      })
+    },
   }]),
 }])
 
@@ -46,11 +74,36 @@ const onTableLoad = defineTableLoad(async ({ update }) => {
   const data = await $request('/api/prompt', { method: 'GET' })
   update(data)
 })
+
+const modal = useModal()
+
+function onCreate(data) {
+  console.log(data)
+}
+
+function onAddClick() {
+  // Bug layout 不起作用
+  modal.open(() => <FormRender form={form2} layout="vertical" footer onSubmit={onCreate}></FormRender>, {
+    onOk: () => {
+      console.log('OK') // TODO foot的确定按钮回调事件定义
+    },
+  }, {
+    title: '弹窗组件',
+    size: 'small',
+    footer: true,
+  })
+}
 </script>
 
 <template>
   <section>
-    <TableRender :columns="columns" :data-load="onTableLoad" row-key="id" />
+    <TableRender ref="promptTable" :columns="columns" :data-load="onTableLoad" row-key="id">
+      <template #actions>
+        <AButton type="primary" @click="onAddClick">
+          增加规则
+        </AButton>
+      </template>
+    </TableRender>
   </section>
 </template>
 
