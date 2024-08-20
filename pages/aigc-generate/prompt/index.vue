8df13c7d4efbb537ca288b2c4eea5ec6db2d38c1
黄瑞 <hr837@163.com>
2024-08-20 17:52:06 +0800

    Huang rui 20240805 5dfed (#3)
    * fix: 测试组件使用
    
    * fix: 试用renderTable,renderForm
    
    * fix: 增加文章列表，标签列表接口获取

--- pages/aigc-generate/prompt/index.vue
+++ pages/aigc-generate/prompt/index.vue
@@ -1,6 +1,23 @@
 <script lang="tsx" setup>
 import { defineColumns, defineTableLoad } from '@gopowerteam/table-render'
+import { FormRender } from '@gopowerteam/form-render'
 import type { Prompt } from '@/drizzle/schemas'
+
+const promptTable = ref(null)
+const form2 = defineForm<Prompt>([
+  {
+    key: 'tag',
+    title: '标签',
+    rule: [{ required: true, message: '请输入标签名称' }],
+    render: r => r.input(),
+  },
+  {
+    key: 'template',
+    title: '模板内容',
+    rule: [{ required: true, message: '请输入模板内容' }],
+    render: r => r.textarea(),
+  },
+])
 
 definePageMeta({
   layout: 'workspace',
@@ -35,10 +52,29 @@ const columns = defineColumns<Prompt>([{
   key: 'action',
   title: '操作',
   render: r => r.button([{
-    content: '更新',
-    // onClick(record) {
-    //   // return requestUpdateTask(record)
-    // },
+    content: '编辑',
+    onClick(record) {
+      // 错误的useTable，需要使用ref
+      promptTable.value.edit({
+        record,
+        form: form2,
+      }).then(async (data) => {
+        await $request(`/api/prompt/${record.id}`, {
+          method: 'PUT',
+          body: data,
+        })
+        Message.success('更新成功')
+      })
+    },
+  }, {
+    content: '删除',
+    onClick(record) {
+      // ?
+      // await $request(`/api/prompt`, {
+
+      // })
+      // Message.success('更新成功')
+    },
   }]),
 }])
 
@@ -46,11 +82,39 @@ const onTableLoad = defineTableLoad(async ({ update }) => {
   const data = await $request('/api/prompt', { method: 'GET' })
   update(data)
 })
+
+const modal = useModal()
+
+function onAddClick() {
+  modal.open(() => <FormRender form={form2} layout="vertical" submitable={true}></FormRender>, {
+    onSubmit: async (data: any) => {
+      await $request('/api/prompt', {
+        method: 'POST',
+        body: data,
+      })
+      modal.close()
+      Message.success('增加成功')
+      promptTable.value.reload()
+    },
+    onCancel: () => {
+      modal.close()
+    },
+  }, {
+    title: '新规则信息',
+    size: 'small',
+  })
+}
 </script>
 
 <template>
   <section>
-    <TableRender :columns="columns" :data-load="onTableLoad" row-key="id" />
+    <TableRender ref="promptTable" :columns="columns" :data-load="onTableLoad" row-key="id">
+      <template #actions>
+        <AButton type="primary" @click="onAddClick">
+          增加规则
+        </AButton>
+      </template>
+    </TableRender>
   </section>
 </template>
 
