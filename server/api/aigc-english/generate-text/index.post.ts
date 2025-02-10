7f75892a58a07581bdaca8f141591d49fc5965f0
zhuchentong <2037630@163.com>
2025-02-11 00:52:49 +0800

    update

--- server/api/aigc-english/generate-text/index.post.ts
+++ server/api/aigc-english/generate-text/index.post.ts
@@ -1,34 +1,33 @@
-import { StructuredOutputParser } from '@langchain/core/output_parsers'
-import { PromptTemplate } from '@langchain/core/prompts'
-import { RunnableSequence } from '@langchain/core/runnables'
+import type { LangChainService } from '~/server/services/langchain'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
 import { eq } from 'drizzle-orm'
 import { z } from 'h3-zod'
 import { SystemSettingFieldsEnum, SystemSettingSchema, SystemSettingScopesEnum } from '~/drizzle/schemas'
-import { LangChainService } from '~/server/services/langchain'
 import { defineAuthEventHandler } from '~/server/utils/define-auth-event-handler'
 
 const PostSchema = z.object({
   topic: z.string(),
 })
 
-async function generateEnglishText(langchain: LangChainService, topic:     string) {
-  // outputParser
-
+async function generateEnglishText(langchain: LangChainService, topic: string, prompt: string) {
   const schema = z.object({
     title: z.string().describe('根据主题生成文章标题'),
-    sentences: z.array(z.object({
-      english: z.string().describe("根据主题生成文章的句子的英文原文"),
-      chinese: z.string().describe("根据主题生成文章的句子的英文原文对应的中文翻译"),
-    })).describe('根据主题生成文章的句子列表'),
+    sentences: z.array(
+      z.object({
+        english: z.string().describe('根据主题生成文章的句子的英文原文'),
+        chinese: z.string().describe('根据主题生成文章的句子的英文原文对应的中文翻译'),
+      }),
+    ).describe('根据主题生成文章的句子列表'),
   })
-  
-  const structuredLlm = langchain.llm.withStructuredOutput(schema);
-  
-  const result  = await structuredLlm.invoke("请根据一下主题生成一篇英文短文，字数在80-100字左右, 在生成英文的同时我需要你帮我生成对应的中文译文。");
-  console.log(result)
-  return result 
 
+  const llm = langchain.llm.withStructuredOutput(schema)
+  const chain = ChatPromptTemplate
+    .fromTemplate(prompt)
+    .pipe(llm)
 
+  return chain.invoke({
+    topic,
+  })
 }
 
 export default defineAuthEventHandler(async (event) => {
@@ -49,10 +48,10 @@ export default defineAuthEventHandler(async (event) => {
   }
 
   const langchain = await useLangchain({
-    modelName:'gpt-3.5-turbo',
+    modelName: 'gpt-3.5-turbo',
   })
 
-  const result = generateEnglishText(langchain, topic)
+  const result = await generateEnglishText(langchain, topic, modelPrompt)
 
   return {
     result,
