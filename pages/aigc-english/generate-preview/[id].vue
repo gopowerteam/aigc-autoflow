aec09ba819769af90a5e0216db10369dbb180bd2
zhuchentong <2037630@163.com>
2025-02-21 21:47:13 +0800

    update

--- pages/aigc-english/generate-preview/[id].vue
+++ pages/aigc-english/generate-preview/[id].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { AigcEnglishContent } from '~/drizzle/schemas';
+import type { AigcEnglishContent } from '~/drizzle/schemas'
 
 definePageMeta({
   layout: 'default',
@@ -12,16 +12,16 @@ const route = useRoute()
 let aigcEnglish = $ref<AigcEnglishContent>()
 let audioContext: AudioContext
 
-let currentTime = $ref(0)
+let currentTime = $ref()
 
-const {pause, resume} = useIntervalFn(()=>{
-  if(audioContext){
-    currentTime = audioContext.currentTime;
+const { pause, resume } = useIntervalFn(() => {
+  if (audioContext) {
+    currentTime = audioContext.currentTime
   }
 }, 100)
 
 async function playAudio() {
-  navigator.mediaDevices.getUserMedia({ audio: true });
+  navigator.mediaDevices.getUserMedia({ audio: true })
 
   audioContext = new AudioContext()
   const audioBuffer = await fetch(aigcEnglish!.audio!)
@@ -58,8 +58,8 @@ async function requestAigcEnglish() {
   const data = await $request('/api/aigc-english/:id', {
     method: 'GET',
     params: {
-      id: route.params.id
-    }
+      id: route.params.id,
+    },
   })
 
   aigcEnglish = data
@@ -71,19 +71,19 @@ const audioDuirationRanges = $computed(() => {
   }
 
   const getDurationSum = (currentIndex: number) => {
-    return aigcEnglish!.sentences .reduce<number>((duration, sentence, index)=>{
+    return aigcEnglish!.sentences.reduce<number>((duration, sentence, index) => {
       if (currentIndex > index) {
         duration += sentence.audioDuration
       }
       return duration
-    },0)
+    }, 0)
   }
 
   const ranges = aigcEnglish.sentences?.map((sentence, index) => {
     const start = getDurationSum(index)
     return {
       start,
-      end: start + sentence.audioDuration
+      end: start + sentence.audioDuration,
     }
   })
 
@@ -92,35 +92,55 @@ const audioDuirationRanges = $computed(() => {
 
 onMounted(async () => {
   await requestAigcEnglish()
-  await playAudio()
-})  
+  window.playAudio = playAudio
+})
 </script>
 
 <template>
-  <section v-if="aigcEnglish" class="absolute inset-0 flex justify-center items-center">
-    <div class="space-y-3 text-18px">
-      <div class="text-center space-y-1" 
-      :class="{ title: index === 0, active: currentTime>= audioDuirationRanges[index].start &&  currentTime <= audioDuirationRanges[index].end  }"
-        v-for="(sentence, index, ) in aigcEnglish?.sentences" :key="sentence.id">
-        <div>{{ sentence.english }}</div>
-        <div>{{ sentence.chinese }}</div>
+  <section v-if="aigcEnglish" class="page-container absolute inset-0 flex items-center justify-center">
+    <div class="space-y-4">
+      <div
+        v-for="(sentence, index,) in aigcEnglish?.sentences"
+        :key="sentence.id"
+        class="text-center space-y-1" :class="{ title: index === 0, active: currentTime >= audioDuirationRanges[index].start && currentTime <= audioDuirationRanges[index].end }"
+      >
+        <div class="english text-20px text-#333">
+          {{ sentence.english }}
+        </div>
+        <div
+          class="chinese text-16px text-#999 tracking-wider"
+        >
+          {{ sentence.chinese }}
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped lang="scss">
+.page-container {
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20'%3E%3Crect width='20' height='0' fill='%23fff' /%3E%3Crect x='50%' width='1' height='100%' fill='rgb(245 245 245)' /%3E%3Crect y='50%' width='100%' height='1' fill='rgb(245 245 245)' /%3E%3C/svg%3E%0A");
+}
+
 .title {
   font-weight: bold;
   font-size: 24px;
   margin-bottom: 20px;
+  color: #000;
 }
 
 .active {
-  background-color: red;
+  background-color: blue;
   border-radius: 10px;
-  color:#fff;
   padding: 10px;
   font-weight: bold;
+
+  .english {
+    color: #fff !important;
+  }
+
+  .chinese {
+    color: #eee !important;
+  }
 }
 </style>
